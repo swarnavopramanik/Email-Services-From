dbdd2c13b6a2d1116d85007cd76f03779c36cd24
Swarnavo Pramanik <105142693+swarnavopramanik@users.noreply.github.com>
2024-09-22 11:34:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1 className='text-center'>Contact Form</h1>
+        <ContactForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;
